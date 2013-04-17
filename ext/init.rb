19f74fed110c1396d3a242e0bae6bf616c2994b9
Fogus <mefogus@gmail.com>
2013-04-17 08:55:02 -0400

    Added Webgen extensions

---
+++ ext/init.rb
@@ -0,0 +1,9 @@
+config = Webgen::WebsiteAccess.website.config
+
+config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.eot'
+config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.woff'
+config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.otf'
+config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.ttf'
+config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.svg'
+config['output'] = ["Webgen::Output::FileSystem", "public"]
+
