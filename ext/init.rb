5f17a49b9d5bd1e56e4850843f013b758656da1a
fogus <mefogus@gmail.com>
2016-07-07 10:10:01 -0400

    Updated to latest Webgen

--- ext/init.rb
+++ ext/init.rb
@@ -1,9 +1,9 @@
-config = Webgen::WebsiteAccess.website.config
+# config = Webgen::WebsiteAccess.website.config
 
-config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.eot'
-config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.woff'
-config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.otf'
-config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.ttf'
-config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.svg'
-config['output'] = ["Webgen::Output::FileSystem", "public"]
+# config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.eot'
+# config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.woff'
+# config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.otf'
+# config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.ttf'
+# config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/*.svg'
+# config['output'] = ["Webgen::Output::FileSystem", "public"]
 
